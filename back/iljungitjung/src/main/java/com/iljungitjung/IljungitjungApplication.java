1eeca989bdc57e41a38c4c04d0aeccd1ca12fee0
goodman1223 <goodman1223@naver.com>
2022-10-17 10:28:10 +0900

    init commit

---
+++ back/iljungitjung/src/main/java/com/iljungitjung/IljungitjungApplication.java
@@ -0,0 +1,13 @@
+package com.iljungitjung;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IljungitjungApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IljungitjungApplication.class, args);
+	}
+
+}
