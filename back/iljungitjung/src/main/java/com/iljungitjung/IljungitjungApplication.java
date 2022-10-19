58444cb0d3f98e2c58ebc433c1fa81e69015fd1e
이기종 <rl00822@naver.com>
2022-10-19 14:39:38 +0900

    [merge] : back-develop ← S07P31D106-14/feature/schedules/view

--- back/iljungitjung/src/main/java/com/iljungitjung/IljungitjungApplication.java
+++ back/iljungitjung/src/main/java/com/iljungitjung/IljungitjungApplication.java
@@ -2,11 +2,14 @@ package com.iljungitjung;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
 public class IljungitjungApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(IljungitjungApplication.class, args);
 	}
 
---
+++ back/iljungitjung/src/main/java/com/iljungitjung/domain/schedule/service/ScheduleServiceImpl.java
@@ -0,0 +1,40 @@
+package com.iljungitjung.domain.schedule.service;
+
+import com.iljungitjung.domain.schedule.dto.ScheduleViewResponseDto;
+import com.iljungitjung.domain.schedule.entity.Schedule;
+import com.iljungitjung.domain.schedule.exception.NoExistScheduleException;
+import com.iljungitjung.domain.schedule.repository.ScheduleRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class ScheduleServiceImpl implements ScheduleService{
+
+    private final ScheduleRepository scheduleRepository;
+
+    @Override
+    public List<ScheduleViewResponseDto> scheduleView(String nickname) {
+
+        //닉네임으로 유저 조회
+        String id = "1";
+        String color = "#000000";
+        List<ScheduleViewResponseDto> responseDtos = new ArrayList<>();
+
+        try{
+            List<Schedule> scheduleList = scheduleRepository.findScheduleByUserToId(id);
+
+            for(Schedule schedule : scheduleList){
+                responseDtos.add(new ScheduleViewResponseDto(schedule, color));
+            }
+
+        }catch (Exception e){
+            throw new NoExistScheduleException();
+        }
+
+        return responseDtos;
+    }
+}
