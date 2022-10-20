b422aef21305aa39ba99fccb16f748f0b6bba4f2
조인후 <inhudev@gmail.com>
2022-10-20 15:13:34 +0900

    S07P31D106-67 [feat] : CustomChip 구현

--- front/src/components/common/CustomChip.tsx
+++ front/src/components/common/CustomChip.tsx
@@ -1,8 +1,33 @@
-const CustomChip = () => {
-  return (
-    <div className="custom-chip">
-    </div>
-  );
+import styles from '@styles/common/Custom.module.scss';
+import { ReactNode, MouseEventHandler } from 'react';
+import Chip from '@mui/material/Chip';
+import { ThemeProvider } from '@mui/material';
+import theme from './theme';
+
+interface ChipProps {
+  label: ReactNode;
+  color?: 'primary' | 'secondary';
+  active?: boolean;
+  onClick?: MouseEventHandler<HTMLDivElement>;
+  onDelete?: () => void;
 }
+
+const CustomChip = ({
+  label,
+  color = 'primary',
+  active = true,
+  ...rest
+}: ChipProps) => {
+  return (
+    <ThemeProvider theme={theme}>
+      <Chip
+        className={active ? '' : styles['half-opacity']}
+        label={label}
+        color={color}
+        {...rest}
+      />
+    </ThemeProvider>
+  );
+};
 
 export default CustomChip;
