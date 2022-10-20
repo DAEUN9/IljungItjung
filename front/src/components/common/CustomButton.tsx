76e3fde2c0d41e6956aacbd65e82ad5baac6cd28
조인후 <joinhu@inuMAC.local>
2022-10-20 15:04:16 +0900

    S07P31D106-64 [feat] : CustomButton 구현

--- front/src/components/common/CustomButton.tsx
+++ front/src/components/common/CustomButton.tsx
@@ -1,8 +1,30 @@
-const CustomButton = () => {
-  return (
-    <div className="custom-button">
-    </div>
-  );
+import { ButtonHTMLAttributes, ReactNode } from 'react';
+import { ThemeProvider } from '@mui/material';
+import Button from '@mui/material/Button';
+import theme from './theme';
+
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  size?: 'small' | 'medium' | 'large';
+  variant?: 'contained' | 'outlined';
+  color?: 'primary' | 'secondary';
+  disabled?: true | false;
+  onClick?: () => void;
 }
+
+const CustomButton = ({
+  size = 'medium',
+  color = 'primary',
+  variant = 'contained',
+  children,
+  ...rest
+}: ButtonProps) => {
+  return (
+    <ThemeProvider theme={theme}>
+      <Button {...rest} size={size} color={color} variant={variant}>
+        {children}
+      </Button>
+    </ThemeProvider>
+  );
+};
 
 export default CustomButton;
