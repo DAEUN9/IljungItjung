625b37f41c070bddbcd55e1a605d90e38f97064d
조인후 <inhudev@gmail.com>
2022-10-20 15:22:34 +0900

    S07P31D106-60 [feat] : CustomModal 구현

--- front/src/components/common/CustomModal.tsx
+++ front/src/components/common/CustomModal.tsx
@@ -1,8 +1,41 @@
-const CustomModal = () => {
+import styles from '@styles/common/Custom.module.scss';
+import { ReactNode } from 'react';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import Modal from '@mui/material/Modal';
+import CustomButton from './CustomButton';
+
+interface ModalProps {
+  open: boolean;
+  setOpen?: (open: boolean) => void;
+  confirmLabel: string;
+  cancelLabel: string;
+  handleConfirm?: () => void;
+  handleCancle?: () => void;
+}
+
+const CustomModal = ({
+  open = true,
+  setOpen,
+  confirmLabel = '확인',
+  cancelLabel = '취소',
+  handleConfirm = undefined,
+  handleCancle = undefined,
+}: ModalProps) => {
   return (
     <div className="custom-modal">
+      <Modal open={open}>
+        <Box className={styles['modal-box']}>
+          <div></div>
+          <div className={styles['button-group']}>
+            <CustomButton color="secondary">{cancelLabel}</CustomButton>
+            <CustomButton>{confirmLabel}</CustomButton>
+          </div>
+        </Box>
+      </Modal>
     </div>
   );
-}
+};
 
 export default CustomModal;
