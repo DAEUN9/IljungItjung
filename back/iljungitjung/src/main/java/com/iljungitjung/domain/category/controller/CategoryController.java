1da8974294b89c0edc20d01526184429fa765ab2
김다은 <asd5687@naver.com>
2022-10-20 14:18:54 +0900

    [merge] : back-develop ← S07P31D106-33/feature/category

---
+++ back/iljungitjung/src/main/java/com/iljungitjung/domain/category/controller/CategoryController.java
@@ -0,0 +1,48 @@
+package com.iljungitjung.domain.category.controller;
+
+import com.iljungitjung.domain.category.dto.CategoryCreateDto;
+import com.iljungitjung.domain.category.dto.CategoryEditDto;
+import com.iljungitjung.domain.category.service.CategoryService;
+import com.iljungitjung.global.common.CommonResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+
+@RequiredArgsConstructor
+@RestController
+@Slf4j
+@RequestMapping(value = "/categories")
+public class CategoryController {
+
+    private final CategoryService categoryService;
+
+    // user생기면 session으로 수정
+    @PostMapping("")
+    public ResponseEntity<CommonResponse> createCategory(
+            @RequestBody CategoryCreateDto requestDto
+    ) {
+        categoryService.addCategory(requestDto);
+        return new ResponseEntity<>(CommonResponse.getSuccessResponse(""), HttpStatus.OK);
+    }
+
+    @PutMapping("")
+    public ResponseEntity<CommonResponse> updateCategory(
+            @RequestBody CategoryEditDto requestDto
+    ) {
+        categoryService.updateCategory(requestDto);
+        return new ResponseEntity<>(CommonResponse.getSuccessResponse(""), HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{category_id}")
+    public ResponseEntity<CommonResponse> deleteCategory(
+            @PathVariable("category_id") int category_id
+    ) {
+        categoryService.deleteCategory(category_id);
+        return new ResponseEntity<>(CommonResponse.getSuccessResponse(""), HttpStatus.OK);
+    }
+
+
+}
---
+++ back/iljungitjung/src/main/java/com/iljungitjung/domain/category/repository/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.iljungitjung.domain.category.repository;
+
+import com.iljungitjung.domain.category.entity.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoryRepository extends JpaRepository<Category, Integer> {
+
+    Category findById(int category_id);
+}
