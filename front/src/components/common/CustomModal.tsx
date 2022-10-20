d014acb72e4dbaf95393cb18e7cf0aad0a45ff21
조인후 <inhudev@gmail.com>
2022-10-20 16:53:09 +0900

    S07P31D106-60 [feat] : content가 있는 CustomModal 스토리 추가

--- front/src/components/common/CustomModal.tsx
+++ front/src/components/common/CustomModal.tsx
@@ -9,8 +9,9 @@ import CustomButton from './CustomButton';
 interface ModalProps {
   open: boolean;
   setOpen?: (open: boolean) => void;
-  confirmLabel: string;
-  cancelLabel: string;
+  confirmLabel?: string;
+  cancelLabel?: string;
+  children?: ReactNode;
   handleConfirm?: () => void;
   handleCancle?: () => void;
 }
@@ -22,12 +23,14 @@ const CustomModal = ({
   cancelLabel = '취소',
   handleConfirm = undefined,
   handleCancle = undefined,
+  children,
+  ...rest
 }: ModalProps) => {
   return (
     <div className="custom-modal">
       <Modal open={open}>
         <Box className={styles['modal-box']}>
-          <div></div>
+          <div>{children}</div>
           <div className={styles['button-group']}>
             <CustomButton color="secondary">{cancelLabel}</CustomButton>
             <CustomButton>{confirmLabel}</CustomButton>
--- front/src/stories/CustomModal.stories.tsx
+++ front/src/stories/CustomModal.stories.tsx
@@ -1,5 +1,7 @@
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 import CustomModal from '@components/common/CustomModal';
+import { Box } from '@mui/material';
+import iljung from '@assets/iljung.png';
 
 export default {
   title: 'Common/CustomModal',
@@ -16,4 +18,30 @@ export const CustomButtonLabel = Template.bind({});
 CustomButtonLabel.args = {
   confirmLabel: '예약 취소',
   cancelLabel: '닫기',
+};
+
+export const ModalWithContent = Template.bind({});
+
+const style = {
+  display: 'flex',
+  justifyContent: 'center',
+};
+
+const ModalContent = () => {
+  return (
+    <Box sx={style}>
+      <div
+        style={{ display: 'flex', flexDirection: 'column', padding: '10px' }}
+      >
+        <div style={{ width: '100px' }}>
+          <img style={{ width: '100%' }} src={iljung} alt="iljung img" />
+        </div>
+        <div>modal content</div>
+      </div>
+    </Box>
+  );
+};
+
+ModalWithContent.args = {
+  children: <ModalContent />,
 };
