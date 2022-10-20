d8fdea32dac42f827afc408bd40eaac26d0cfb12
nullyng <nullyng@gmail.com>
2022-10-20 20:15:19 +0900

    S07P31D106-53 [feat] : 각 페이지에 Sidebar 연결
    - index.html 스타일 body margin 0으로 수정
    - _uitls.modules.scss 파일에 primary color 및 페이지 기본 스타일 mixin 정의
    - 각 페이지 컴포넌트에 Sidebar 연결

--- front/src/pages/ReservationPage.tsx
+++ front/src/pages/ReservationPage.tsx
@@ -1,8 +1,13 @@
+import Sidebar from "@components/common/Sidebar";
+import styles from "@styles/Reservation/Reservation.module.scss";
+
 const ReservationPage = () => {
   return (
-    <div className="reservation">
+    <div className={styles["reservation-page"]}>
+      <Sidebar />
+      <div className={styles["content"]}></div>
     </div>
   );
-}
+};
 
 export default ReservationPage;
--- front/src/pages/SearchPage.tsx
+++ front/src/pages/SearchPage.tsx
@@ -1,5 +1,13 @@
+import Sidebar from "@components/common/Sidebar";
+import styles from "@styles/Search/Search.module.scss";
+
 const SearchPage = () => {
-  return <div className="search"></div>;
+  return (
+    <div className={styles["search-page"]}>
+      <Sidebar />
+      <div className={styles["content"]}></div>
+    </div>
+  );
 };
 
 export default SearchPage;
