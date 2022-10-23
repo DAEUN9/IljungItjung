c75b7dd4dde6cb406681c2f4be4fa733e2f25803
조인후 <inhudev@gmail.com>
2022-10-23 23:00:12 +0900

    S07P31D106-69 [feat] : CustomTooltip 구현

--- front/src/components/common/CustomTooltip.tsx
+++ front/src/components/common/CustomTooltip.tsx
@@ -1,8 +1,41 @@
-const CustomTooltip = () => {
+import styled from '@emotion/styled';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import Tooltip, { tooltipClasses, TooltipProps } from '@mui/material/Tooltip';
+
+// 받아야하는 props: title, children, 툴팁 위치(required)
+interface customTooltipProps extends TooltipProps {}
+
+const StyledTooltip = styled(({ className, ...props }: TooltipProps) => (
+  <Tooltip {...props} classes={{ popper: className }} />
+))`
+  & .${tooltipClasses.tooltip} {
+    font-size: 12px;
+    border: 1px solid #dadada;
+    background-color: #eaeaea;
+    color: black;
+  }
+
+  & .${tooltipClasses.arrow} {
+    color: #eaeaea;
+
+    &:before {
+      border: 1px solid #dadada;
+    }
+  }
+`;
+
+const CustomTooltip = ({
+  title,
+  children,
+  placement,
+  ...rest
+}: customTooltipProps) => {
   return (
-    <div className="custom-tooltip">
-    </div>
+    <StyledTooltip title={title} placement={placement} arrow {...rest}>
+      {children}
+    </StyledTooltip>
   );
-}
+};
 
 export default CustomTooltip;
