7c426942480ec2b7a45b2ec411c8a8284e88c8e5
김주영 <wn00315@naver.com>
2022-10-24 17:28:03 +0900

    S07P31D106-50/feature/common 재사용 가능한 컴포넌트를 구현한다

--- front/src/components/common/Schedule.tsx
+++ front/src/components/common/Schedule.tsx
@@ -1,6 +1,50 @@
-const Schedule = () => {
+import { useEffect } from "react";
+
+import styles from "@styles/common/Schedule.module.scss";
+
+interface ScheduleProps {
+  color: string,
+  date?: string,
+  time: string,
+  userId: string,
+  userImg: string,
+  userName: string,
+  category: string,
+  render?: () => JSX.Element
+}
+
+const Schedule = ({ color, date = "", time, userId, userImg, userName, category, render }: ScheduleProps) => {
+  useEffect(() => {
+    const el = document.querySelector(".color") as HTMLElement;
+    if (el) el.style.backgroundColor = color;
+  }, [])
+
+  const handleClickProfile = () => {
+    // 해당 유저의 페이지로 이동
+    window.location.href = `/calendar/${userId}`;
+  }
+
   return (
-    <div className="schedule">
+    <div className={styles["schedule"]}>
+      <div className={styles["top"]}>
+        <div className={styles["left"]}>
+          <div className={`${styles["color"]} color`}></div>
+          <div className={styles["dateTime"]}>
+            {date &&
+              <span className={styles["date"]}>{date}&nbsp;</span>
+            }
+            <span className={styles["time"]}>{time}</span>
+          </div>
+        </div>
+        <div className={styles["right"]} onClick={handleClickProfile}>
+          <img src={userImg} />
+          <div>{userName}</div>
+        </div>
+      </div>
+      <div className={styles["bottom"]}>
+        <div className={styles["category"]}>{category}</div>
+        {render && render()}
+      </div>
     </div>
   );
 }
