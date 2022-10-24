a5b0290031d4217a5634a8161218446c69f2f4a6
InhuJo <inhudev@gmail.com>
2022-10-24 17:34:32 +0900

    S07P31D106-52 [feat] : 달력 컴포넌트 날짜 형식 변경

--- front/src/components/Calendar/MyCalendar.tsx
+++ front/src/components/Calendar/MyCalendar.tsx
@@ -1,34 +1,51 @@
+import React, { useEffect } from 'react';
+import ReactDOM from 'react-dom/client'
 import 'devextreme/dist/css/dx.light.css';
 import { Scheduler, View, Scrolling } from 'devextreme-react/scheduler';
+import IconButton from '@mui/material/IconButton';
+import SettingsIcon from '@mui/icons-material/Settings';
 
 import styles from '@styles/Calendar/Calendar.module.scss';
+import '@styles/Calendar/CustomCalendar.css';
 
 const dayOfWeekNames = ['일', '월', '화', '수', '목', '금', '토'];
 
 const MyCalendar = () => {
+  useEffect(() => {
+    const setting = document.createElement('div');
+    setting.className = 'setting';
+    setting.innerText = 'setting';
+    const el = document.getElementsByClassName('dx-toolbar-before')[0];
+    el.appendChild(setting);
+    // const settingBtn = React.createElement(IconButton);
+    // ReactDOM.createRoot(el as HTMLElement).render(<IconButton><SettingsIcon /></IconButton>)
+  }, [])
+
   return (
     <Scheduler
-      className={styles['my-calendar']}
+      className={styles.calendar}
       defaultCurrentView="week"
       timeZone="Asia/Seoul"
       showAllDayPanel={false}
-      views={['week']}
       startDayHour={9}
       endDayHour={22}
+      height={'100%'}
+      width={'63%'}
     >
-      <View type="week" dateCellRender={renderDateCell} />
+      <View type="week" dateCellRender={renderDateCell} firstDayOfWeek={1} />
       <Scrolling mode="virtual" />
     </Scheduler>
   );
 };
 
-function renderDateCell(cellData: any) {
-  console.log(cellData);
+function renderDateCell(cellData: any) {  
   return (
-    <>
-      <div className="name">{dayOfWeekNames[cellData.date.getDay()]}</div>
-      <div className="number">{cellData.date.getDate()}</div>
-    </>
+    <div className={styles['date-cell']}>
+      <div>
+        <span className={styles.number}>{cellData.date.getDate()}</span>
+        <span className={styles.name}>{dayOfWeekNames[cellData.date.getDay()]}</span>
+      </div>
+    </div>
   );
 }
 
