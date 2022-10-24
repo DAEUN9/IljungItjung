902015b44a90fabbb74e6f515c21f083edc7c641
InhuJo <inhudev@gmail.com>
2022-10-24 16:03:29 +0900

    S07P31D106-52 [feat] : 달력 컴포넌트 추가
    - devExtreme 라이브러리의 scheduler 컴포넌트 사용

--- front/src/components/Calendar/MyCalendar.tsx
+++ front/src/components/Calendar/MyCalendar.tsx
@@ -1,7 +1,34 @@
+import 'devextreme/dist/css/dx.light.css';
+import { Scheduler, View, Scrolling } from 'devextreme-react/scheduler';
+
+import styles from '@styles/Calendar/Calendar.module.scss';
+
+const dayOfWeekNames = ['일', '월', '화', '수', '목', '금', '토'];
+
 const MyCalendar = () => {
   return (
-    <div className="my-calendar">
-    </div>
+    <Scheduler
+      className={styles['my-calendar']}
+      defaultCurrentView="week"
+      timeZone="Asia/Seoul"
+      showAllDayPanel={false}
+      views={['week']}
+      startDayHour={9}
+      endDayHour={22}
+    >
+      <View type="week" dateCellRender={renderDateCell} />
+      <Scrolling mode="virtual" />
+    </Scheduler>
+  );
+};
+
+function renderDateCell(cellData: any) {
+  console.log(cellData);
+  return (
+    <>
+      <div className="name">{dayOfWeekNames[cellData.date.getDay()]}</div>
+      <div className="number">{cellData.date.getDate()}</div>
+    </>
   );
 }
 
