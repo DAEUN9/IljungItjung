b590dd587568b2d75a2be993761e1fc736323687
nullyng <nullyng@gmail.com>
2022-10-25 17:53:59 +0900

    S07P31D106-51 [feat] : 예약 목록 아이템 컴포넌트 구현
    - 취소 여부, 만료 여부에 따라 알맞은 일정 컴포넌트를 출력하도 함

--- front/src/components/Reservation/Reservation.tsx
+++ front/src/components/Reservation/Reservation.tsx
@@ -22,11 +22,123 @@ interface ReservationProps {
   list: ListType[];
 }
 
+interface CancelButtonProps {
+  detail: string;
+}
+
+// 취소 버튼
+const CancelButton = ({ detail }: CancelButtonProps) => {
+  return (
+    <div className={styles["cancel-button"]}>
+      <div className={styles["detail"]}>{detail}</div>
+      <div className={styles["button"]}>
+        <Button startIcon={<DeleteForeverOutlinedIcon />}>취소하기</Button>
+      </div>
+    </div>
+  );
+};
+
+// 취소 사유
+interface CancelReasonProps {
+  reason: string;
+  detail: string;
+}
+
+const CancelReason = ({ reason, detail }: CancelReasonProps) => {
+  return (
+    <div className={styles["cancel-reason"]}>
+      <div className={styles["detail"]}>{detail}</div>
+      <div className={styles["reason-wraaper"]}>{reason}</div>
+    </div>
+  );
+};
+
+/**
+ * 만료된 예약인지 아닌지 계산
+ * @returns 만료되었다면 true, 그렇지 않다면 false
+ */
+const calculateDate = (date: string, time: string): boolean => {
+  const today = new Date();
+  return false;
+};
+
 const Reservation = ({ date, list }: ReservationProps) => {
   return (
     <div className={styles.reservation}>
       <div className={styles.date}>{date}</div>
-      <div className={styles.list}></div>
+      <div className={styles.list}>
+        {list.map((item, index) => {
+          // 취소하거나 취소된 예약이면
+          if (item.isCanceled) {
+            if (item.cancelReason) {
+              return (
+                <Schedule
+                  key={index}
+                  isCanceled={true}
+                  color={item.color}
+                  time={item.time}
+                  userId={item.userId}
+                  userImg={item.userImg}
+                  userName={item.userName}
+                  category={item.category}
+                  render={() => (
+                    <CancelReason
+                      reason={item.cancelReason as string}
+                      detail={item.detail}
+                    />
+                  )}
+                />
+              );
+            } else {
+              return (
+                <Schedule
+                  key={index}
+                  isCanceled={true}
+                  color={item.color}
+                  time={item.time}
+                  userId={item.userId}
+                  userImg={item.userImg}
+                  userName={item.userName}
+                  category={item.category}
+                  render={() => (
+                    <div className={styles["detail"]}>{item.detail}</div>
+                  )}
+                />
+              );
+            }
+          } else {
+            if (calculateDate(date, item.time)) {
+              return (
+                <Schedule
+                  key={index}
+                  color={item.color}
+                  time={item.time}
+                  userId={item.userId}
+                  userImg={item.userImg}
+                  userName={item.userName}
+                  category={item.category}
+                  render={() => (
+                    <div className={styles["detail"]}>{item.detail}</div>
+                  )}
+                />
+              );
+            } else {
+              return (
+                <Schedule
+                  key={index}
+                  color={item.color}
+                  time={item.time}
+                  userId={item.userId}
+                  userImg={item.userImg}
+                  userName={item.userName}
+                  category={item.category}
+                  render={() => <CancelButton detail={item.detail} />}
+                />
+              );
+            }
+          }
+        })}
+      </div>
     </div>
   );
 };
