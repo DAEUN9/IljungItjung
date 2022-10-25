e13fc22f7961cb401f5e889acc0d9853be788368
InhuJo <inhudev@gmail.com>
2022-10-25 17:51:11 +0900

    S07P31D106-56 [feat] : 캘린더 컴포넌트 라이브러리 변경
    - devexpress/dx-react-scheduler-material-ui 사용

--- front/src/components/Calendar/MyCalendar.tsx
+++ front/src/components/Calendar/MyCalendar.tsx
@@ -1,10 +1,18 @@
-import { ComponentType, ButtonHTMLAttributes, useState } from 'react';
+import { ComponentType, ReactNode, useState } from 'react';
 import Paper from '@mui/material/Paper';
-import { ViewState, TodayButton } from '@devexpress/dx-react-scheduler';
+import Avatar from '@mui/material/Avatar';
+import AvatarGroup from '@mui/material/AvatarGroup';
+import IconButton from '@mui/material/IconButton';
+import SettingsIcon from '@mui/icons-material/Settings';
+import {
+  ViewState,
+  TodayButton,
+  Toolbar,
+} from '@devexpress/dx-react-scheduler';
 import {
   Scheduler,
   WeekView,
-  Toolbar,
+  Toolbar as MuiToolbar,
   DateNavigator,
   Appointments,
   TodayButton as MuiTodayButton,
@@ -13,7 +21,7 @@ import {
 import styles from '@styles/Calendar/Calendar.module.scss';
 import '@styles/Calendar/CustomCalendar.css';
 
-interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+interface ButtonProps {
   /** Function that sets the Scheduler's current date. */
   setCurrentDate: (nextDate: Date) => void;
   /** Returns a localization message by the message key. */
@@ -23,8 +31,41 @@ interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   [x: string]: any;
 }
 
-const StyledTodayButton: ComponentType<ButtonProps> = (props: ButtonProps) => (
+interface RootProps {
+  /** A React node to be placed in the toolbar. */
+  children?: React.ReactNode;
+}
+
+interface FlexibleSpaceProps {
+  /** A React node that should be placed inside the empty area. */
+  children?: React.ReactNode;
+}
+
+const CustomTodayButton: ComponentType<ButtonProps> = (props) => (
   <MuiTodayButton.Button {...props} className={styles.today} />
+);
+
+const CustomToolbarRoot: ComponentType<RootProps> = ({ children }) => (
+  <MuiToolbar.Root>{children}</MuiToolbar.Root>
+);
+
+const CustomToolbarFlexibleSpace: ComponentType<FlexibleSpaceProps> = ({
+  children,
+}) => <MuiToolbar.FlexibleSpace>{children}</MuiToolbar.FlexibleSpace>;
+
+const CustomerList = () => (
+  <AvatarGroup
+    max={4}
+    sx={{
+      '& .MuiAvatar-root': { width: 30, height: 30, fontSize: 15 },
+    }}
+  >
+    <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
+    <Avatar alt="Travis Howard" src="/static/images/avatar/2.jpg" />
+    <Avatar alt="Agnes Walker" src="/static/images/avatar/4.jpg" />
+    <Avatar alt="Trevor Henderson" src="/static/images/avatar/5.jpg" />
+    <Avatar alt="Trevor Henderson" src="/static/images/avatar/5.jpg" />
+  </AvatarGroup>
 );
 
 const schedulerData = [
@@ -50,16 +91,30 @@ const MyCalendar = () => {
 
   return (
     <Paper className={styles['calendar-container']}>
-      <Scheduler data={schedulerData} locale="ko-KR">
+      <Scheduler data={schedulerData} locale="ko-KR" firstDayOfWeek={1}>
         <ViewState
           currentDate={currentDate}
           onCurrentDateChange={(currentDate) => setCurrentDate(currentDate)}
         />
         <WeekView startDayHour={9} endDayHour={22} />
-        <Toolbar />
+        <Toolbar
+          rootComponent={CustomToolbarRoot}
+          flexibleSpaceComponent={() => (
+            <CustomToolbarFlexibleSpace
+              children={
+                // <div className={styles['customer-list']}>
+                //   <IconButton>
+                //     <SettingsIcon />
+                //   </IconButton>
+                // </div>
+                <CustomerList />
+              }
+            />
+          )}
+        />
         <TodayButton
           messages={{ today: '오늘' }}
-          buttonComponent={StyledTodayButton}
+          buttonComponent={CustomTodayButton}
         />
         <DateNavigator />
         <Appointments />
