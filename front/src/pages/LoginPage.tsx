d8ac5a31d3789f5ad7aa8c6a1f6d6004e3e921cb
SSAFY <wodud5381@naver.com>
2022-10-26 13:06:15 +0900

    [feat] : 유저기능 UI 구현 - api 통신 제외

--- front/src/pages/LoginPage.tsx
+++ front/src/pages/LoginPage.tsx
@@ -1,6 +1,21 @@
+import CustomButton from "@components/common/CustomButton";
+import CustomCarousel from "@components/Login/CustomCarousel";
+import styles from "@styles/Login/Login.module.scss";
+import iljung from "@assets/iljung.png";
+import logo from "@assets/logo.png";
+
 const LoginPage = () => {
   return (
-    <div className="login">
+    <div className={styles["login-page"]}>
+      <div className={styles["left"]}>
+        <img src={logo} alt="로고" className={styles["logo"]} />
+        <img src={iljung} alt="일정이" className={styles["iljung"]} />
+      </div>
+      <div className={styles["right"]}>
+        <div className={styles["welcome"]}>일정있정에 오신 것을 환영합니다.</div>
+        <CustomCarousel />
+        <CustomButton size="large" color="secondary" className={styles["login"]}>임시 카카오 로그인</CustomButton>
+      </div>
     </div>
   );
 }
