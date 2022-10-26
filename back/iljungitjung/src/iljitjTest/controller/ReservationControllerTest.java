12797ff6dd239eba9a672d35d8b2cb214fff5e18
rl00822 <rl00822@naver.com>
2022-10-26 10:27:41 +0900

    S07P31D106 [feat] : reservation service 테스트 코드 작성

--- back/iljungitjung/src/iljitjTest/controller/ReservationControllerTest.java
+++ back/iljungitjung/src/iljitjTest/controller/ReservationControllerTest.java
@@ -1,4 +1,5 @@
 import com.iljungitjung.domain.category.dto.CategoryCreateRequestDto;
+import com.iljungitjung.domain.schedule.dto.reservation.ReservationBlockRequestDto;
 import com.iljungitjung.domain.schedule.dto.reservation.ReservationManageRequestDto;
 import com.iljungitjung.domain.schedule.dto.reservation.ReservationRequestDto;
 import org.junit.jupiter.api.MethodOrderer;
@@ -54,13 +55,13 @@ public class ReservationControllerTest extends AbstractControllerTest{
 
     @Test
     @Order(3)
-    public void 일정_삭제() throws Exception {
+    public void 일정_수락() throws Exception {
 
-        String content = objectMapper.writeValueAsString(new ReservationManageRequestDto(false, "시간이 없어요"));
+        String content = objectMapper.writeValueAsString(new ReservationManageRequestDto(true, "가능합니다. 잘 부탁드려요."));
 
 
         //given
-        ResultActions actions = mockMvc.perform(put("/reservations/1")
+        ResultActions actions = mockMvc.perform(put("/reservations/" + scheduleId)
                 .content(content)
                 .contentType(MediaType.APPLICATION_JSON)
                 .accept(MediaType.APPLICATION_JSON));
@@ -69,8 +70,46 @@ public class ReservationControllerTest extends AbstractControllerTest{
         actions.andDo(print())
                 .andExpect(status().isOk());
     }
+
+
     @Test
     @Order(4)
+    public void 일정_삭제() throws Exception {
+
+        String content = objectMapper.writeValueAsString(new ReservationManageRequestDto(false, "시간이 없어요"));
+
+
+        //given
+        ResultActions actions = mockMvc.perform(put("/reservations/" + scheduleId)
+                .content(content)
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON));
+
+        //then
+        actions.andDo(print())
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    @Order(5)
+    public void 일정_차단_요청() throws Exception {
+
+        String content = objectMapper.writeValueAsString(new ReservationBlockRequestDto(
+                "1", "공휴일", "공휴일이라서 쉽니다.", "20221017", "1500", "1630"));
+        //given
+        ResultActions actions = mockMvc.perform(post("/reservations/block")
+                .content(content)
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON));
+
+        //then
+        actions.andDo(print())
+                .andExpect(status().isOk());
+        scheduleId++;
+    }
+
+    @Test
+    @Order(6)
     public void 카테고리_삭제() throws Exception {
 
         //given
