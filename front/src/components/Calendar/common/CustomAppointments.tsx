c05ac0b876615e53ffe185102b2c0b60f98bb819
InhuJo <inhudev@gmail.com>
2022-10-27 15:00:05 +0900

    S07P31D106-58 [feat] : 캘린더 일정 컴포넌트 구현

--- front/src/components/Calendar/common/CustomAppointments.tsx
+++ front/src/components/Calendar/common/CustomAppointments.tsx
@@ -1,5 +1,37 @@
 import { Appointments } from '@devexpress/dx-react-scheduler-material-ui';
+import Avatar from '@mui/material/Avatar';
+
+import styles from '@styles/Calendar/Calendar.module.scss';
 
 export default function CustomAppointments() {
-  return <Appointments />
+  return (
+    <Appointments
+      appointmentComponent={(props) => (
+        <Appointments.Appointment
+          {...props}
+          style={{ backgroundColor: '#D7CBF4' }}
+        />
+      )}
+      appointmentContentComponent={(props) => (
+        <Appointments.AppointmentContent
+          {...props}
+          children={
+            <>
+              <div className={styles['appointment-category']}>
+                카테고리명이 길면 우째
+              </div>
+              <div className={styles['appointment-nickname']}>
+                <Avatar sx={{ width: '18px', height: '18px' }} />
+                <div className={styles['appointment-nickname-wrapper']}>
+                  <div>
+                    <span style={{ fontWeight: 600 }}>nickname</span> 님
+                  </div>
+                </div>
+              </div>
+            </>
+          }
+        />
+      )}
+    />
+  );
 }
