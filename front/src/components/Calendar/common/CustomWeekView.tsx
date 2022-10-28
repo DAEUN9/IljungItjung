bd54e97208ff31a255cdc383c916be166922c061
InhuJo <inhudev@gmail.com>
2022-10-28 17:37:03 +0900

    S07P31D106-52 [asset] : 선택 불가능한 일정 설정에 필요한 lock, unlcok asset 추가

--- front/src/components/Calendar/common/CustomWeekView.tsx
+++ front/src/components/Calendar/common/CustomWeekView.tsx
@@ -2,8 +2,14 @@ import { WeekView } from '@devexpress/dx-react-scheduler-material-ui';
 
 import { getDay } from './util';
 import styles from '@styles/Calendar/Calendar.module.scss';
+import { useEffect } from 'react';
 
 export default function CustomWeekView() {
+  useEffect(() => {
+    const els = document.getElementsByClassName('Cell-dayView');
+    /* 맨처음 or navigator click 이벤트마다 자물쇠 렌더링 */
+  }, []);
+
   return (
     <WeekView
       startDayHour={9}
