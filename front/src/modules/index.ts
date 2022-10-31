97ad80ac959ea15a513d4d5ab7a59b7c93ce907a
nullyng <nullyng@gmail.com>
2022-10-31 15:36:24 +0900

    S07P31D106-95 [feat] : 검색 관련 redux 코드 추가
    - 검색된 아이템 중 선택한 사용자 정보를 state로 저장 및 관리

--- front/src/modules/index.ts
+++ front/src/modules/index.ts
@@ -1,9 +1,13 @@
 import { combineReducers } from "redux";
 
 import reservation from "@modules/reservation";
+import search from "@modules/search";
 
 const rootReducer = combineReducers({
   reservation,
+  search,
 });
 
 export default rootReducer;
+
+export type RootState = ReturnType<typeof rootReducer>;
