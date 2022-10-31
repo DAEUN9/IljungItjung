bc855eee4890cb9e6355af5967d9c54d4231af0f
goodman1223 <goodman1223@naver.com>
2022-10-31 15:02:24 +0900

    [fix] : webserver -> ${WEBSERVER_NAME}

--- back/iljungitjung/deploy.sh
+++ back/iljungitjung/deploy.sh
@@ -1,6 +1,7 @@
 docker build -t iljungitjung_server .
 
 DOCKER_APP_NAME=iljungitjung_server
+WEBSERVER_NAME=iljungitjung_webserver_1
 
 EXIST_BLUE=$(docker-compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.blue.yml ps | grep Up)
 
@@ -10,9 +11,9 @@ if [ -z "$EXIST_BLUE" ]; then
 	sleep 20
 	START_PORT=9090
 	TERMINATE_PORT=9091
-	docker exec webserver sed -i "s/${TERMINATE_PORT}/${START_PORT}/" /etc/nginx/conf.d/default.conf
+	docker exec ${WEBSERVER_NAME} sed -i "s/${TERMINATE_PORT}/${START_PORT}/" /etc/nginx/conf.d/default.conf
 	echo "nginx reload..."
-  docker exec webserver service nginx reload
+  docker exec ${WEBSERVER_NAME} service nginx reload
 	sleep 5
 	docker-compose -p ${DOCKER_APP_NAME}-green -f docker-compose.green.yml down
 else
@@ -21,9 +22,9 @@ else
 	sleep 20
 	START_PORT=9091
 	TERMINATE_PORT=9090
-	docker exec webserver sed -i "s/${TERMINATE_PORT}/${START_PORT}/" /etc/nginx/conf.d/default.conf
+	docker exec ${WEBSERVER_NAME} sed -i "s/${TERMINATE_PORT}/${START_PORT}/" /etc/nginx/conf.d/default.conf
 	echo "nginx reload..."
-	docker exec webserver service nginx reload
+	docker exec ${WEBSERVER_NAME} service nginx reload
 	sleep 5
 	docker-compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.blue.yml down
 fi
