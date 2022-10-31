20fd63a299de717ee9dba46ea7407f3cf4aa451a
SSAFY <wodud5381@naver.com>
2022-10-31 09:56:33 +0900

    Squashed commit of the following:
    commit b5d1877e1bc31e8559f5b0562be8a55505bdc986
    Merge: e4d3b46 54ea2a7
    Author: 김주영 <wn00315@naver.com>
    Date:   Thu Oct 27 14:16:54 2022 +0900
    
        [merge] : front-develop ← S07P31D106-50/feature/common
    
        Merge branch 'S07P31D106-50/feature/common' into 'front-develop'
    
        S07P31D106-50/feature/common 재사용 가능한 컴포넌트를 구현한다
    
        See merge request s07-final/S07P31D106!17
    
    commit 54ea2a79e8b2bf57f1605ad73f678c54aec26100
    Author: 김주영 <wn00315@naver.com>
    Date:   Thu Oct 27 14:16:54 2022 +0900
    
        S07P31D106-50/feature/common 재사용 가능한 컴포넌트를 구현한다

--- front/src/pages/ProfileEditPage.tsx
+++ front/src/pages/ProfileEditPage.tsx
@@ -3,39 +3,67 @@ import Sidebar from "@components/common/Sidebar";
 import styles from "@styles/ProfileEdit/ProfileEdit.module.scss";
 import defaultImg from "@assets/defaultImg.png";
 import PictureEdit from "@assets/PictureEdit.png";
-import { useCallback, useState } from "react";
+import React, { useState } from "react";
 import CustomModal from "@components/common/CustomModal";
 
+import { ref, getDownloadURL, uploadBytes, uploadBytesResumable } from "firebase/storage"
+import { storage } from "src/firebase";
+
 const ProfileEditPage = () => {
+
+  // 한 줄 소개
   const [text, setText] = useState("");
   const count = (event : React.ChangeEvent<HTMLInputElement>) => {
     setText(event.target.value)
   }
 
+  // 설명
   const [explain, setexplain] = useState("");
   const count2 = (event : React.ChangeEvent<HTMLTextAreaElement>) => {
     setexplain(event.target.value)
   }
 
-  const [isOpenModal, setOpenModal] = useState<boolean>(false);
+  // 변경 후 저장 Modal
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleConfirm = () => // API 저장 요청 
+  void(0)
 
-  const onClickToggleModal = useCallback(() => {
-    setOpenModal(!isOpenModal);
-  }, [isOpenModal]);
-
+  // 프로필 사진 업로드 Modal
+  const [openPic, setOpenPic] = React.useState(false);
+  const handleOpenPic = () => setOpenPic(true);
+  const handleConfirmPic = () => // 사진 저장 요청
+  void(0)
 
   return (
     <div className={styles["profile-edit-page"]}>
       <Sidebar />
-      <CustomModal open={isOpenModal} />
+      <CustomModal open={open} setOpen={setOpen} handleConfirm={handleConfirm} 
+      children={
+      <div className={styles["editmodal"]}>
+        <div className={styles["picturebox"]}>
+          <img src={defaultImg} className={styles["picture"]} />
+        </div>
+        <div className={styles["textbox"]}>
+          <p>회원 정보가 수정됩니다.</p>
+          <p>계속 하시겠습니까?</p>
+        </div>
+      </div>
+      }/>
+      <CustomModal open={openPic} setOpen={setOpenPic} handleConfirm={handleConfirmPic} 
+      children={
+        <div>
+          <input type="file" />
+        </div>
+      } />
       <div className={styles["left"]}>
         <div>
           <div className={styles["pictureBox"]}>
-            <img className={styles["pictureEdit"]} src={PictureEdit} alt="사진 변경" />
+            <img className={styles["pictureEdit"]} src={PictureEdit} alt="사진 변경" onClick={handleOpenPic} />
             <img className={styles["picture"]} src={defaultImg} alt="임시사진" />
           </div>
           <h1 className={styles["name"]}>임시 이름</h1>
-          <CustomButton size="large" onClick={onClickToggleModal}>저장하기</CustomButton>
+          <CustomButton size="large" onClick={handleOpen}>저장하기</CustomButton>
         </div>
       </div>
       <div className={styles["right"]}>
