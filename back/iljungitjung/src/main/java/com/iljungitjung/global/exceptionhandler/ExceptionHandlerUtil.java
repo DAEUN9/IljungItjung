41e452e24c724c1c2bf11b96f5903bdc3001be7f
rl00822 <rl00822@naver.com>
2022-11-01 14:29:13 +0900

    [feat] : ExceptionHandlerUtil 추가 및 수정

--- back/iljungitjung/src/main/java/com/iljungitjung/global/exceptionhandler/ExceptionHandlerUtil.java
+++ back/iljungitjung/src/main/java/com/iljungitjung/global/exceptionhandler/ExceptionHandlerUtil.java
@@ -1,6 +1,8 @@
 package com.iljungitjung.global.exceptionhandler;
 
 import com.iljungitjung.domain.category.exception.NoExistCategoryException;
+import com.iljungitjung.domain.schedule.exception.DateFormatErrorException;
+import com.iljungitjung.domain.schedule.exception.NoExistScheduleDetailException;
 import com.iljungitjung.domain.schedule.exception.NoExistScheduleException;
 import com.iljungitjung.global.common.CommonResponse;
 import com.iljungitjung.global.login.exception.NotMemberException;
@@ -20,8 +22,16 @@ public class ExceptionHandlerUtil {
     ResponseEntity<CommonResponse> handleNoExistScheduleException(BindingResult bindingResult){
         return ResponseEntity.badRequest().body(CommonResponse.getFailResponse(bindingResult));
     }
+    @ExceptionHandler(NoExistScheduleDetailException.class)
+    ResponseEntity<CommonResponse> v(BindingResult bindingResult) {
+        return ResponseEntity.badRequest().body(CommonResponse.getFailResponse(bindingResult));
+    }
+    @ExceptionHandler(DateFormatErrorException.class)
+    ResponseEntity<CommonResponse> handleDateFormatErrorException(BindingResult bindingResult) {
+        return ResponseEntity.badRequest().body(CommonResponse.getFailResponse(bindingResult));
+    }
 
-    @ExceptionHandler(value={NoExistCategoryException.class})
+    @ExceptionHandler(NoExistCategoryException.class)
     ResponseEntity<CommonResponse> handleNoExistCategoryException(BindingResult bindingResult) {
         return ResponseEntity.badRequest().body(CommonResponse.getFailResponse(bindingResult));
     }
@@ -30,4 +40,5 @@ public class ExceptionHandlerUtil {
     ResponseEntity<CommonResponse> handleNotMemberException(NotMemberException e){
         return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(CommonResponse.getErrorResponse(e.getMessage()));
     }
+
 }
