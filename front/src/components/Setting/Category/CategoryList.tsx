01a6aad970207533c4684e0453c2b171c305760e
nullyng <nullyng@gmail.com>
2022-11-02 17:50:58 +0900

    S07P31D106-73 [feat] : 카테고리 추가 및 삭제 이벤트 등록

--- front/src/components/Setting/Category/CategoryList.tsx
+++ front/src/components/Setting/Category/CategoryList.tsx
@@ -1,9 +1,54 @@
+import { useEffect, useRef, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Snackbar } from "@mui/material";
+
 import CustomChip from "@components/common/CustomChip";
 import styles from "@styles/Setting/CategoryList.module.scss";
+import { addCategory, delCategory, setCategory } from "@modules/setting";
+import { CategoryState } from "@components/types/types";
+import { RootState } from "@modules/index";
 
-const categories = ["커트", "목욕", "손발톱관리"];
+const data: CategoryState[] = [
+  // { name: "커트", color: "#FF0000", hour: "2", min: "30" },
+  // { name: "손발톱관리", color: "#000000", hour: "1", min: "00" },
+  // { name: "목욕", color: "#00FF00", hour: "3", min: "00" },
+];
 
 const CategoryList = () => {
+  const categories = useSelector(
+    (state: RootState) => state.setting.categories
+  );
+
+  const dispatch = useDispatch();
+  const onSetCategory = (categories: CategoryState[]) =>
+    dispatch(setCategory(categories));
+  const onAddCategory = (category: CategoryState) =>
+    dispatch(addCategory(category));
+  const onDelCategory = (category: CategoryState) =>
+    dispatch(delCategory(category));
+
+  const [delSnackbar, setDelSnackbar] = useState(false);
+
+  // const colorRef = useRef<HTMLDivElement[]>([]);
+
+  useEffect(() => {
+    if (data.length > 0) onSetCategory(data);
+    // colorRef.current.map((curr) => {
+    //   const index = curr.className.lastIndexOf("#");
+    //   const color = curr.className.substring(index);
+    //   const el = curr.childNodes[0] as HTMLDivElement;
+    //   el.style.backgroundColor = color;
+    // });
+  }, []);
+
+  const handleDeleteCategory = (category: CategoryState) => {
+    if (categories.length < 2) {
+      setDelSnackbar(true);
+    } else {
+      onDelCategory(category);
+    }
+  };
+
   return (
     <div className={styles["category-list"]}>
       <div className={styles["title"]}>
@@ -12,14 +57,25 @@ const CategoryList = () => {
       </div>
       <div className={styles["content"]}>
         {categories.map((category, index) => (
-          <div className={styles["chip"]} key={index}>
+          <div
+            className={`${styles["chip"]} ${category.color}`}
+            key={index}
+            // ref={(el) => (colorRef.current[index] = el as HTMLDivElement)}
+          >
             <CustomChip
-              label={category}
+              label={category.name}
               onClick={() => {}}
-              onDelete={() => {}}
+              onDelete={() => handleDeleteCategory(category)}
             />
           </div>
         ))}
+        <Snackbar
+          open={delSnackbar}
+          autoHideDuration={6000}
+          anchorOrigin={{ vertical: "top", horizontal: "right" }}
+          onClose={() => setDelSnackbar(false)}
+          message="카테고리는 1개 이상 있어야 합니다."
+        />
       </div>
     </div>
   );
--- front/src/pages/SettingPage.tsx
+++ front/src/pages/SettingPage.tsx
@@ -3,6 +3,7 @@ import { Scheduler } from "@devexpress/dx-react-scheduler-material-ui";
 import React, { useState } from "react";
 import { IoHelpCircleOutline } from "react-icons/io5";
 import { IconButton, Tab, Tabs } from "@mui/material";
+import { useNavigate } from "react-router-dom";
 
 import Sidebar from "@components/common/Sidebar";
 import styles from "@styles/Setting/Setting.module.scss";
@@ -18,6 +19,8 @@ import CustomTooltip from "@components/common/CustomTooltip";
 import ChangeTab from "@components/Setting/Category/ChangeTab";
 import AddTab from "@components/Setting/Category/AddTab";
 import "@styles/Setting/TabButton.scss";
+import CustomModal from "@components/common/CustomModal";
+import iljung from "@assets/iljung.png";
 
 const data = [
   {
@@ -71,6 +74,24 @@ function TabPanel(props: TabPanelProps) {
 const SettingPage = () => {
   const [currentDate, setCurrentDate] = useState(new Date());
   const [tab, setTab] = useState(0);
+  const navigate = useNavigate();
+
+  const [saveOpen, setSaveOpen] = useState(false);
+  const [cancelOpen, setCancelOpen] = useState(false);
+
+  const handleClickSave = () => {
+    // 모달 띄우기
+    setSaveOpen(true);
+  };
+
+  const handleClickCancel = () => {
+    navigate("/calendar/my");
+  };
+
+  const handleSubmit = () => {
+    console.log("handleSubmit()");
+    // 변경 내용 api 요청
+  };
 
   const handleTabChange = (e: React.SyntheticEvent, newValue: number) => {
     setTab(newValue);
@@ -96,9 +117,36 @@ const SettingPage = () => {
         </div>
         <div className={styles["right"]}>
           <div className={styles["button-group"]}>
-            <CustomButton variant="contained" children="저장" />
-            <CustomButton color="secondary" children="취소" />
+            <CustomButton
+              variant="contained"
+              children="저장"
+              onClick={handleClickSave}
+            />
+            <CustomButton
+              color="secondary"
+              children="취소"
+              onClick={handleClickCancel}
+            />
           </div>
+          <CustomModal
+            open={saveOpen}
+            setOpen={setSaveOpen}
+            cancelLabel="취소"
+            confirmLabel="저장"
+            handleConfirm={handleSubmit}
+            children={
+              <div className={styles["modal-content"]}>
+                <div className={styles["img"]}>
+                  <img src={iljung} />
+                </div>
+                <div className={styles["text"]}>
+                  변경사항이 저장됩니다.
+                  <br />
+                  계속 하시겠습니까?
+                </div>
+              </div>
+            }
+          />
           <div className={styles["category-container"]}>
             <div className={styles["title"]}>
               <h2>카테고리 관리</h2>
