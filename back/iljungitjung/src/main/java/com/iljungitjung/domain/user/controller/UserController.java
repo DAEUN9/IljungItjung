5bc3e6fada53928c7994ce9d9df2be517c90bfbf
goodman1223 <goodman1223@naver.com>
2022-11-02 17:16:56 +0900

    S07P31D106-10 [feat] : 사용자 정보 얻어오는 기능 추가

--- back/iljungitjung/src/main/java/com/iljungitjung/domain/user/controller/UserController.java
+++ back/iljungitjung/src/main/java/com/iljungitjung/domain/user/controller/UserController.java
@@ -5,16 +5,18 @@ import com.iljungitjung.domain.user.service.UserService;
 import com.iljungitjung.global.common.CommonResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import javax.validation.constraints.Pattern;
+import java.util.Objects;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/users")
+@Validated
 public class UserController {
 
     private final UserService userService;
@@ -22,5 +24,11 @@ public class UserController {
     @PostMapping
     public void signUpUser(@RequestBody SignUpDto signUpDto, HttpServletRequest request){
         userService.signUpUser(signUpDto, request);
+    }
+
+    @GetMapping
+    public ResponseEntity<CommonResponse> getUserInfo (@Pattern(regexp = "^[a-z|A-Z|0-9|ㄱ-ㅎ|가-힣]{2,10}$", message = "닉네임은 최소 2자, 최대 10자 영어, 한글, 숫자만 입력가능합니다.") @RequestParam(name = "nickname", required = false) String nickname, HttpSession session){
+        if(Objects.isNull(nickname)) return ResponseEntity.ok(CommonResponse.getSuccessResponse(userService.getUserInfo(session)));
+        return ResponseEntity.ok(CommonResponse.getSuccessResponse(userService.getUserInfo(nickname)));
     }
 }
