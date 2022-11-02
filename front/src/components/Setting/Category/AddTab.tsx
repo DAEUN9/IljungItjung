638ccc2ea59fef8c0cde0ab3dc8d65e8ccdebd29
nullyng <nullyng@gmail.com>
2022-11-02 17:51:44 +0900

    S07P31D106-75 [feat] : Controller 추가
    - react-hook-form의 Controller 추가

--- front/src/components/Setting/Category/AddTab.tsx
+++ front/src/components/Setting/Category/AddTab.tsx
@@ -4,14 +4,19 @@ import {
   MenuItem,
   Select,
   SelectChangeEvent,
+  TextField,
 } from "@mui/material";
 import React, { useState } from "react";
 import { CirclePicker } from "react-color";
 import { ThemeProvider } from "@emotion/react";
+import { useDispatch } from "react-redux";
+import { Controller, useForm } from "react-hook-form";
 
 import styles from "@styles/Setting/Tab.module.scss";
 import CustomButton from "@components/common/CustomButton";
 import theme from "@components/common/theme";
+import { CategoryState } from "@components/types/types";
+import { addCategory } from "@modules/setting";
 
 const hours = [
   "0",
@@ -48,36 +53,68 @@ const colorList = [
   "#D8DFF1",
 ];
 
+interface FormValue {
+  name: string;
+  hour: string;
+  min: string;
+}
+
 const AddTab = () => {
-  const [categoryName, setCategoryName] = useState("");
-  const [hour, setHour] = useState("1");
-  const [min, setMin] = useState("00");
-  const [value, setValue] = useState(1);
+  const { register, handleSubmit, watch, control } = useForm();
+  const [color, setColor] = useState("#D5EAEF");
 
-  const handleChangeCategory = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setCategoryName(event.target.value);
+  const dispatch = useDispatch();
+  const onAddCategory = (category: CategoryState) =>
+    dispatch(addCategory(category));
+
+  const handleClickSubmit = (data: FormValue) => {
+    console.log(data);
+    // if (category.name || category.name !== "") {
+    //   onAddCategory(category);
+    //   setCategory;
+    // }
   };
-
-  const handleChangeHour = (event: SelectChangeEvent) => {
-    setHour(event.target.value);
-  };
-
-  const handleChangeMin = (event: SelectChangeEvent) => {
-    setMin(event.target.value);
-  };
-
-  const handleClickSubmit = () => {};
 
   return (
     <div className={styles["tab"]}>
       <ThemeProvider theme={theme}>
         <div className={styles["category-name"]}>
           <h3>카테고리명</h3>
-          <Input
+          <Controller
+            name="name"
+            control={control}
+            rules={{ required: "카테고리명을 입력해 주세요." }}
+            render={({
+              field: { onChange, value },
+              fieldState: { isDirty, error },
+            }) => (
+              <TextField
+                size="small"
+                variant="standard"
+                placeholder="카테고리명을 입력해주세요."
+                inputProps={{ maxLength: 15 }}
+                fullWidth
+                value={value}
+                onChange={onChange}
+                error={!!error}
+                helperText={isDirty && `${value.length}/15`}
+              />
+            )}
+          />
+          {/* <TextField
+            size="small"
+            variant="standard"
+            error={isSubmitted && (!categoryName || categoryName === "")}
+            inputProps={{ maxLength: 15 }}
             placeholder="카테고리명을 입력해주세요."
             onChange={handleChangeCategory}
             className={styles["text-field"]}
-          />
+            helperText={
+              isSubmitted &&
+              (!categoryName || categoryName === "") &&
+              "카테고리명을 입력해주세요."
+            }
+          /> */}
         </div>
         <div className={styles["time-taken"]}>
           <div className={styles["title"]}>
@@ -85,43 +122,65 @@ const AddTab = () => {
             <span>* 30분 단위로 입력</span>
           </div>
           <div className={styles["dropdown"]}>
-            <FormControl className={styles["hour"]} size="small">
-              <Select
-                label=""
-                className={styles["select"]}
-                value={hour}
-                onChange={handleChangeHour}
-                displayEmpty
-                inputProps={{ "aria-label": "Without label" }}
-              >
-                {hours.map((hour, index) => (
-                  <MenuItem key={index} value={hour}>
-                    {hour}
-                  </MenuItem>
-                ))}
-              </Select>
-            </FormControl>
+            <Controller
+              name="hour"
+              control={control}
+              render={({ field: { onChange, value } }) => (
+                <FormControl className={styles["hour"]} size="small">
+                  <Select
+                    label=""
+                    className={styles["select"]}
+                    value={value}
+                    onChange={onChange}
+                    displayEmpty
+                  >
+                    {hours.map((hour, index) => (
+                      <MenuItem key={index} value={hour}>
+                        {hour}
+                      </MenuItem>
+                    ))}
+                  </Select>
+                </FormControl>
+              )}
+            />
             <span>시간</span>
-            <FormControl className={styles["min"]} size="small">
-              <Select
-                className={styles["select"]}
-                value={min}
-                onChange={handleChangeMin}
-                displayEmpty
-              >
-                <MenuItem value={"00"}>00</MenuItem>
-                <MenuItem value={"30"}>30</MenuItem>
-              </Select>
-            </FormControl>
+            <Controller
+              name="min"
+              control={control}
+              render={({ field: { onChange, value } }) => (
+                <FormControl className={styles["min"]} size="small">
+                  <Select
+                    className={styles["select"]}
+                    value={value}
+                    onChange={onChange}
+                    displayEmpty
+                  >
+                    <MenuItem value={"00"}>00</MenuItem>
+                    <MenuItem value={"30"}>30</MenuItem>
+                  </Select>
+                </FormControl>
+              )}
+            />
+
             <span>분</span>
           </div>
         </div>
         <div className={styles["category-color"]}>
           <h3>카테고리 색상</h3>
-          <CirclePicker className={styles["color-picker"]} colors={colorList} />
+          <CirclePicker
+            className={styles["color-picker"]}
+            colors={colorList}
+            color={color}
+            onChange={(color) => {
+              setColor(color.hex);
+            }}
+          />
         </div>
         <div className={styles["submit-button"]}>
-          <CustomButton children="완료" onClick={handleClickSubmit} />
+          <CustomButton
+            children="완료"
+            onSubmit={handleSubmit(handleClickSubmit)}
+          />
         </div>
       </ThemeProvider>
     </div>
