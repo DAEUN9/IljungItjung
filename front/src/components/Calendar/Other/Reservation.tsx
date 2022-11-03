c250925e0c6271bd6c5ab259902adce81b11d199
InhuJo <inhudev@gmail.com>
2022-11-03 13:17:27 +0900

    S07P31D106-88 [feat] : 예약 요청 폼에 React Hook Form 적용

--- front/src/components/Calendar/Other/Reservation.tsx
+++ front/src/components/Calendar/Other/Reservation.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { useSelector } from 'react-redux';
-import { useForm, Controller } from 'react-hook-form';
+import { useForm, Controller, SubmitHandler } from 'react-hook-form';
 import styled from '@emotion/styled';
 import MuiInputLabel from '@mui/material/InputLabel';
 import OutlinedInput from '@mui/material/OutlinedInput';
@@ -34,6 +34,7 @@ const TextField = styled(MuiTextField)`
 const PhoneTextField = styled(TextField)`
   > .MuiInputBase-root > input {
     padding: 7px;
+    padding-left: 12px;
   }
 `;
 
@@ -44,49 +45,58 @@ const Select = styled(MuiSelect)`
 `;
 
 const InputLabel = styled(MuiInputLabel)`
+  color: rgba(0, 0, 0, 0.5);
+
   &.Mui-focused {
     color: #6b7bb1;
   }
-`
+`;
 
 const Reservation = () => {
   const { handleSubmit, control } = useForm();
   const current = useSelector((state: RootState) => state.othercalendar.time);
-  const [age, setAge] = useState('');
 
-  const handleChange = (event: SelectChangeEvent) => {
-    setAge(event.target.value as string);
-  };
-
-  const onSubmit = (data: RequestData) => console.log(data);
+  const onSubmit: SubmitHandler<RequestData> = data => console.log(data);
 
   return (
     <div className={styles.reservation}>
       {!current && <div className={styles.center}>시간대를 선택해주세요</div>}
       {current && (
-        <form>
+        <form onSubmit={handleSubmit(onSubmit)}>
           <div className={styles['reservation-inner']}>
             <div className={styles['reservation-item']}>
               <div className={styles.center}>
                 <FaThList />
               </div>
-              <FormControl fullWidth>
-                <InputLabel id="select-label" size="small">카테고리 선택</InputLabel>
-                <Select
-                  labelId="select-label"
-                  id="select"
-                  value={age}
-                  label="카테고리 선택"
-                  input={
-                    <OutlinedInput id="select-category" label="카테고리 선택" />
-                  }
-                  size="small"
-                >
-                  <MenuItem value={10}>Ten</MenuItem>
-                  <MenuItem value={20}>Twenty</MenuItem>
-                  <MenuItem value={30}>Thirty</MenuItem>
-                </Select>
-              </FormControl>
+              <Controller
+                control={control}
+                name="category"
+                defaultValue=""
+                render={({ field }) => (
+                  <FormControl fullWidth>
+                    <InputLabel id="select-label" size="small">
+                      카테고리 선택
+                    </InputLabel>
+                    <Select
+                      labelId="select-label"
+                      id="select"
+                      label="카테고리 선택"
+                      input={
+                        <OutlinedInput
+                          id="select-category"
+                          label="카테고리 선택"
+                        />
+                      }
+                      size="small"
+                      {...field}
+                    >
+                      <MenuItem value={10}>Ten</MenuItem>
+                      <MenuItem value={20}>Twenty</MenuItem>
+                      <MenuItem value={30}>Thirty</MenuItem>
+                    </Select>
+                  </FormControl>
+                )}
+              />
             </div>
             <div className={styles['reservation-item']}>
               <div className={styles.center}>
@@ -104,11 +114,25 @@ const Reservation = () => {
               <div className={styles.center}>
                 <FaPhoneAlt />
               </div>
-              <PhoneTextField placeholder="연락처" />
+              <Controller
+                control={control}
+                name="phone"
+                defaultValue=""
+                render={({ field }) => (
+                  <PhoneTextField placeholder="연락처" {...field} />
+                )}
+              />
             </div>
             <div className={styles['reservation-request']}>
               <div>요청사항</div>
-              <TextField fullWidth multiline rows={2} />
+              <Controller
+                control={control}
+                name="request"
+                defaultValue=""
+                render={({ field }) => (
+                  <TextField fullWidth multiline rows={2} {...field} />
+                )}
+              />
             </div>
             <CustomButton
               style={{ width: 'calc(100% - 10px)', margin: '0 5px' }}
