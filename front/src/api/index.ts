15ff64d9c1a8addc246df441000ccb903dba49a8
nullyng <nullyng@gmail.com>
2022-11-07 17:40:37 +0900

    [chore] : axios headers 수정

--- front/src/api/index.ts
+++ front/src/api/index.ts
@@ -3,8 +3,8 @@ import axios from "axios";
 const apiInstance = () => {
   const instance = axios.create({
     baseURL: import.meta.env.VITE_API_BASE_URL,
-    headers: { 
-      "Content-type": "application/json",
+    headers: {
+      Accept: "application/json",
     },
   });
 
