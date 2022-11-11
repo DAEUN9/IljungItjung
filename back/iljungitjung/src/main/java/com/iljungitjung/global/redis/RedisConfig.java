a88e3ec5f149b5ce4369e82b9173f05b329fdec0
goodman1223 <goodman1223@naver.com>
2022-11-11 14:40:27 +0900

    [fix] : REDIS PASSWORD 추가

--- back/iljungitjung/src/main/java/com/iljungitjung/global/redis/RedisConfig.java
+++ back/iljungitjung/src/main/java/com/iljungitjung/global/redis/RedisConfig.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 
 @Configuration
@@ -14,8 +15,15 @@ public class RedisConfig {
     @Value("${spring.redis.port}")
     private int PORT;
 
+    @Value("${spring.redis.password}")
+    private String PASSWORD;
+
     @Bean
     public RedisConnectionFactory redisConnectionFactory(){
-        return new LettuceConnectionFactory(HOST, PORT);
+        RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration();
+        redisStandaloneConfiguration.setHostName(HOST);
+        redisStandaloneConfiguration.setPort(PORT);
+        redisStandaloneConfiguration.setPassword(PASSWORD);
+        return new LettuceConnectionFactory(redisStandaloneConfiguration);
     }
 }
