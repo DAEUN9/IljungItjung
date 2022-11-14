59eca532d3b8aeb85ea3ef92c429d81fd85b9c14
nullyng <nullyng@gmail.com>
2022-11-14 10:15:19 +0900

    [fix] : 충돌 해결

--- front/src/components/Calendar/My/UpcomingItem.tsx
+++ front/src/components/Calendar/My/UpcomingItem.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import styles from "@styles/Calendar/Calendar.module.scss";
 import Schedule from "@components/common/Schedule";
 import iljung from "@assets/defaultImg.png";
@@ -8,20 +7,12 @@ import {
   getFullDate,
   InfoItemProps,
 } from "@components/Calendar/common/util";
-=======
-import styles from '@styles/Calendar/Calendar.module.scss';
-import Schedule from '@components/common/Schedule';
-import iljung from '@assets/defaultImg.png';
-import DetailInfo from './DetailInfo';
-import { formatTime, getFullDate, InfoItemProps } from '@components/Calendar/common/util';
->>>>>>> front-develop
 
 const UpcomingItem = ({ item }: InfoItemProps) => {
   const { color, startDate, endDate, title, nickname, phone, desc } = item;
   const time = formatTime(startDate?.toString(), endDate?.toString());
 
   return (
-<<<<<<< HEAD
     <div className={styles["info-item"]}>
       <Schedule
         color={color}
@@ -30,16 +21,6 @@ const UpcomingItem = ({ item }: InfoItemProps) => {
         userId="유저아이디"
         userName={nickname}
         category={title ?? "-"}
-=======
-    <div className={styles['info-item']}>
-      <Schedule
-        color={color}
-        date={getFullDate(new Date(startDate.toString()))}
-        time={time ?? '-'}
-        userId="유저아이디"
-        userName={nickname}
-        category={title ?? '-'}
->>>>>>> front-develop
         userImg={iljung}
         render={() => <DetailInfo phone={phone} desc={desc} />}
       />
--- front/src/components/Setting/SettingWeekView.tsx
+++ front/src/components/Setting/SettingWeekView.tsx
@@ -1,9 +1,6 @@
 import { WeekView } from "@devexpress/dx-react-scheduler-material-ui";
 import { useDispatch, useSelector } from "react-redux";
-<<<<<<< HEAD
-=======
 import { useEffect } from "react";
->>>>>>> front-develop
 
 import {
   getDay,
@@ -25,11 +22,8 @@ export default function SettingWeekView() {
   const onDeleteLockShade = (day: number, time: string, all?: boolean) =>
     dispatch(deleteLockShade(day, time, all));
 
-<<<<<<< HEAD
-=======
   useEffect(() => {}, [lockMap]);
 
->>>>>>> front-develop
   return (
     <WeekView
       startDayHour={9}
