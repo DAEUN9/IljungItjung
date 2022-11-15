892f9678618f1f4ffe684d1c20e963e19ef5368c
rl00822 <rl00822@naver.com>
2022-11-15 12:54:29 +0900

    S07P31D106-163 [feat] : category 관련 API 수정 요청사항 적용 후 testcode 수정

--- back/iljungitjung/src/main/java/com/iljungitjung/domain/category/dto/CategoryListCreateRequestDto.java
+++ back/iljungitjung/src/main/java/com/iljungitjung/domain/category/dto/CategoryListCreateRequestDto.java
@@ -1,10 +1,12 @@
 package com.iljungitjung.domain.category.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 import java.util.List;
 
 @Getter
+@AllArgsConstructor
 public class CategoryListCreateRequestDto {
     private List<CategoryCreateRequestDto> categoryList;
 
