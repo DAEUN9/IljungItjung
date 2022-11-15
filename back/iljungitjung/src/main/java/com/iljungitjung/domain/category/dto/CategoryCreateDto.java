8894e2efa8e9f7482a0ac9d4246bfa980328d58f
이기종 <rl00822@naver.com>
2022-11-15 19:24:46 +0900

    [fix] : category 관련 유효성 검사 수정

--- back/iljungitjung/src/main/java/com/iljungitjung/domain/category/dto/CategoryCreateDto.java
+++ back/iljungitjung/src/main/java/com/iljungitjung/domain/category/dto/CategoryCreateDto.java
@@ -15,7 +15,7 @@ import javax.validation.constraints.Size;
 public class CategoryCreateDto {
 
     @NotBlank(message = "categoryName은 필수 입력 값입니다.")
-    @Pattern(regexp = "^[a-z|A-Z|0-9|ㄱ-ㅎ|가-힣]{2,10}$", message = "카테고리 이름은 최소 2자, 최대 10자 영어, 한글, 숫자만 입력가능합니다.")
+    @Pattern(regexp = "^[a-z|A-Z|0-9|ㄱ-ㅎ|가-힣]{2,20}$", message = "카테고리 이름은 최소 2자, 최대 20자 영어, 한글, 숫자만 입력가능합니다.")
     private String categoryName;
 
     @Size(min=4, max=4, message = "time 형식을 맞춰주세요 (ex.0130)")
@@ -23,7 +23,6 @@ public class CategoryCreateDto {
     private String time;
 
     @Size(min=7, max=7, message = "color 형식을 맞춰주세요 (ex.#000000)")
-    @Pattern(regexp = "^#[0-9]+$", message = "color 형식을 맞춰주세요 (ex.#000000)")
     private String color;
 
     public Category toEntity(){
