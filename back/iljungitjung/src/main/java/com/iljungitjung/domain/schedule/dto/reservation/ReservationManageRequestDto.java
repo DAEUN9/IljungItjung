70f6408df1400cc450929a695a18b0020d9d4d5a
이기종 <rl00822@naver.com>
2022-11-16 21:18:34 +0900

    S07P31D106-166 [refactor] : 유효성 검증 부분 추가 작성

--- back/iljungitjung/src/main/java/com/iljungitjung/domain/schedule/dto/reservation/ReservationManageRequestDto.java
+++ back/iljungitjung/src/main/java/com/iljungitjung/domain/schedule/dto/reservation/ReservationManageRequestDto.java
@@ -15,7 +15,7 @@ public class ReservationManageRequestDto {
     @NotNull(message = "accept는 true 또는 false 입니다.")
     private boolean accept;
 
-    @Size(min = 0, max = 100, message = "한줄소개는 최소 0자, 최대 100자만 가능합니다.")
+    @Size(min = 0, max = 100, message = "취소 이유는 최소 0자, 최대 100자만 가능합니다.")
     private String reason;
 
 }
