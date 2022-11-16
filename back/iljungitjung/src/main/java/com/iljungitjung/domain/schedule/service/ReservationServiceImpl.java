c5b3ae01fc350908498e8147ca3439277366c6c1
daeun <gain3436@gmail.com>
2022-11-16 16:11:37 +0900

    [style] : reservationServiceImpl에 notificationservice오타 수정

--- back/iljungitjung/src/main/java/com/iljungitjung/domain/schedule/service/ReservationServiceImpl.java
+++ back/iljungitjung/src/main/java/com/iljungitjung/domain/schedule/service/ReservationServiceImpl.java
@@ -33,7 +33,7 @@ public class ReservationServiceImpl implements ReservationService{
 
     private final UserRepository userRepository;
     private final UserService userService;
-    private final NotificationService notificasionService;
+    private final NotificationService notificationService;
 
     @Override
     @Transactional
@@ -71,7 +71,7 @@ public class ReservationServiceImpl implements ReservationService{
         schedule.setScheduleResponseList(userTo);
 
         schedule = scheduleRepository.save(schedule);
-        notificasionService.autoReservationMessage(schedule);
+        notificationService.autoReservationMessage(schedule);
         return new ReservationIdResponseDto(schedule.getId());
     }
 
@@ -104,7 +104,7 @@ public class ReservationServiceImpl implements ReservationService{
         }else{
             throw new NoGrantAccessScheduleException();
         }
-        notificasionService.autoReservationMessage(schedule);
+        notificationService.autoReservationMessage(schedule);
         return new ReservationIdResponseDto(schedule.getId());
     }
 
@@ -120,7 +120,7 @@ public class ReservationServiceImpl implements ReservationService{
         if(!checkSamePerson(user, schedule.getUserTo())) throw new NoGrantDeleteScheduleException();
 
         schedule.deleted();
-        notificasionService.autoReservationMessage(schedule);
+        notificationService.autoReservationMessage(schedule);
     }
 
     @Override
