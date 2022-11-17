bb51016459fd609173db82af4a71526113c48b52
daeun <gain3436@gmail.com>
2022-11-18 08:56:16 +0900

    [feat] : 전화번호 인증 login interceptor에 true처리

--- back/iljungitjung/src/main/java/com/iljungitjung/domain/notification/controller/PhoneController.java
+++ back/iljungitjung/src/main/java/com/iljungitjung/domain/notification/controller/PhoneController.java
@@ -9,7 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
-import javax.servlet.http.HttpSession;
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 import javax.validation.constraints.Pattern;
 
@@ -19,16 +19,15 @@ import javax.validation.constraints.Pattern;
 @Validated
 public class PhoneController {
     private final PhoneService phoneService;
-    private final String EXPIRATION = "인증번호가 만료되었습니다.";
 
-    @GetMapping("/{phonenum}")
+    @PostMapping("/{phonenum}")
     public ResponseEntity<CommonResponse> sendAuthenticatePhone (
             @Pattern(regexp = "^01\\d{8,9}$", message = "전화번호는 01로 시작하고 10~11자리의 숫자만 입력가능합니다.")
             @PathVariable("phonenum")
             String phonenum,
-            HttpSession httpSession
+            HttpServletRequest request
     ) {
-        String response = phoneService.requestRandomNumber(phonenum, httpSession);
+        String response = phoneService.requestRandomNumber(phonenum, request.getSession());
         if(response.length() == 6) {
             return new ResponseEntity<>(CommonResponse.getSuccessResponse(response), HttpStatus.OK);
         }
@@ -39,8 +38,8 @@ public class PhoneController {
     public void confirmAuthenticatePhone (
             @Valid @RequestBody
             PhoneConfirmRequestDto requestDto,
-            HttpSession httpSession
+            HttpServletRequest request
     ) {
-        phoneService.confirmRandomNumber(requestDto, httpSession);
+        phoneService.confirmRandomNumber(requestDto, request.getSession());
     }
 }
