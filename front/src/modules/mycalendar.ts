cb14ac82d25505848f00a9bd7d322cd1bee973ed
InhuJo <inhudev@gmail.com>
2022-11-17 09:32:38 +0900

    S07P31D106-80 [feat] : 요청 수락/거절에 필요한 action 추가
    - ADD_SCHEDULE : 일정 리스트에 일정 추가
    - DELETE_REQUEST : 요청 목록에서 아이템 삭제

--- front/src/modules/mycalendar.ts
+++ front/src/modules/mycalendar.ts
@@ -4,6 +4,7 @@ import { SchedulerDate } from "@components/types/types";
 const SET_SCHEDULE_LIST = "mycalendar/SET_SCHEDULE_LIST" as const;
 const SET_REQUEST_LIST = "mycalendar/SET_REQUEST_LIST" as const;
 const ADD_SCHEDULE = "mycalendar/ADD_SCHEDULE" as const;
+const DELETE_REQUEST = "mycalendar/DELETE_REQUEST" as const;
 
 /* action creator */
 export const setScheduleList = (list: SchedulerDate[]) => ({
@@ -21,10 +22,22 @@ export const addSchedule = (schedule: SchedulerDate) => ({
   payload: schedule,
 });
 
+export const deleteRequest = (id: number) => ({
+  type: DELETE_REQUEST,
+  payload: id,
+});
+
+/* function */
+const filterRequest = (request: SchedulerDate[], id: number) => {
+  const filtered = request.filter((item) => item.id !== id);
+  return filtered;
+};
+
 type MyCalendarAction =
   | ReturnType<typeof setScheduleList>
   | ReturnType<typeof setRequestList>
-  | ReturnType<typeof addSchedule>;
+  | ReturnType<typeof addSchedule>
+  | ReturnType<typeof deleteRequest>;
 
 export interface MyCalendarState {
   list: SchedulerDate[];
@@ -44,7 +57,14 @@ export default function reducer(
       return { ...state, request: action.payload };
     case ADD_SCHEDULE:
       const list = state.list.concat([action.payload]);
-      return { ...state, list };
+      return {
+        ...state,
+        list,
+        request: filterRequest(state.request, action.payload.id),
+      };
+    case DELETE_REQUEST:
+      const request = filterRequest(state.request, action.payload);
+      return { ...state, request };
     default:
       return state;
   }
