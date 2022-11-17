e19f4064869359771d2810aebbb64c9ed18bc67f
SSAFY <wodud5381@naver.com>
2022-11-17 10:00:10 +0900

    S07P31D106-110 [merge] : S07P31D106-110 <- front-develop

--- front/src/api/index.ts
+++ front/src/api/index.ts
@@ -4,6 +4,7 @@ const apiInstance = () => {
   const instance = axios.create({
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
     baseURL: "http://localhost:5173/api",
     headers: {
       Accept: "application/json",
@@ -12,6 +13,11 @@ const apiInstance = () => {
     headers: {
       "Content-type": "application/json",
 >>>>>>> 653a39ec835717dd46a43023cbbfaa11b42ac025
+=======
+    baseURL: import.meta.env.VITE_API_BASE_URL,
+    headers: {
+      Accept: "application/json",
+>>>>>>> 5d8f8f59d8ec19d8971dbe9e724ebc3c5f8058a5
 =======
     baseURL: import.meta.env.VITE_API_BASE_URL,
     headers: {
---
+++ front/src/components/Setting/SettingWeekView.tsx
@@ -0,0 +1,103 @@
+import { WeekView } from "@devexpress/dx-react-scheduler-material-ui";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+
+import {
+  getDay,
+  getFullStringFromDate,
+  makeFormat,
+} from "@components/Calendar/common/util";
+import styles from "@styles/Setting/Setting.module.scss";
+import { RootState } from "@modules/index";
+import { deleteLockShade, lockShade, toggleShade } from "@modules/setting";
+
+export default function SettingWeekView() {
+  const { lock, set, lockMap } = useSelector(
+    (state: RootState) => state.setting
+  );
+  const dispatch = useDispatch();
+  const onToggleShade = (date: string) => dispatch(toggleShade(date));
+  const onLockShade = (day: number, time: string) =>
+    dispatch(lockShade(day, time));
+  const onDeleteLockShade = (day: number, time: string, all?: boolean) =>
+    dispatch(deleteLockShade(day, time, all));
+
+  return (
+    <WeekView
+      startDayHour={9}
+      endDayHour={22}
+      dayScaleCellComponent={(props) => (
+        <WeekView.DayScaleCell
+          {...props}
+          className="day-scale"
+          formatDate={getDay}
+        />
+      )}
+      timeTableCellComponent={(props) => {
+        let isDisabled = false;
+
+        if (props.startDate && props.endDate) {
+          isDisabled = set.has(
+            getFullStringFromDate(props.startDate, props.endDate)
+          );
+
+          const day = props.startDate.getDay();
+          if (lock[(day + 6) % 7]) {
+            const list = lockMap.get(day);
+            if (list) {
+              for (const item of list) {
+                const propsTime =
+                  makeFormat(props.startDate.getHours().toString()) +
+                  makeFormat(props.startDate.getMinutes().toString());
+                if (item === propsTime) {
+                  isDisabled = true;
+                  break;
+                }
+              }
+            }
+          }
+        }
+
+        return (
+          <WeekView.TimeTableCell
+            key="hi"
+            {...props}
+            className={styles["time-table-cell"]}
+            children={
+              <div
+                className={
+                  isDisabled
+                    ? `${styles["cell"]} ${styles["shade"]}`
+                    : styles["cell"]
+                }
+                onClick={() => {
+                  const date = getFullStringFromDate(
+                    props.startDate as Date,
+                    props.endDate as Date
+                  );
+                  onToggleShade(date);
+
+                  if (props.startDate) {
+                    const day = props.startDate.getDay();
+
+                    // 해당 요일이 잠금된 상태라면, lockMap에도 추가한다.
+                    if (lock[(day + 6) % 7]) {
+                      const propsTime =
+                        makeFormat(props.startDate.getHours().toString()) +
+                        makeFormat(props.startDate.getMinutes().toString());
+
+                      // 이미 lockMap에 존재한다면 지우고, 그렇지 않다면 추가한다.
+                      if (lockMap.get(day)?.includes(propsTime))
+                        onDeleteLockShade(day, propsTime);
+                      else onLockShade(day, propsTime);
+                    }
+                  }
+                }}
+              />
+            }
+          />
+        );
+      }}
+    />
+  );
+}
