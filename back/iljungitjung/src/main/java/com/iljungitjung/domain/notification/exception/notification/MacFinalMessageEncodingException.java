0d9deac453e096ac5107fd7f598af65bf5ed108e
daeun <gain3436@gmail.com>
2022-11-18 01:58:35 +0900

    [refactor] : 미사용 exception 제거

--- back/iljungitjung/src/main/java/com/iljungitjung/domain/notification/exception/notification/MacFinalMessageEncodingException.java
+++
@@ -1,5 +0,0 @@
-package com.iljungitjung.domain.notification.exception.notification;
-
-public class MacFinalMessageEncodingException extends RuntimeException{
-    public MacFinalMessageEncodingException(){ super("MAC 종료 메시지를 인코딩 할 수 없습니다.");}
-}
--- back/iljungitjung/src/main/java/com/iljungitjung/domain/notification/exception/notification/SecretKeyEncodingException.java
+++
@@ -1,5 +0,0 @@
-package com.iljungitjung.domain.notification.exception.notification;
-
-public class SecretKeyEncodingException extends RuntimeException{
-    public SecretKeyEncodingException(){ super("Secret Key를 인코딩 할 수 없습니다.");}
-}
