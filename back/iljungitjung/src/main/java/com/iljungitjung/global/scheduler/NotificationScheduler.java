dc435b38e412236e63462abd4a8cc2e04f2c43ba
김다은 <asd5687@naver.com>
2022-11-20 06:05:18 +0900

    [merge] : back <- back-develop

--- back/iljungitjung/src/main/java/com/iljungitjung/global/scheduler/NotificationScheduler.java
+++ back/iljungitjung/src/main/java/com/iljungitjung/global/scheduler/NotificationScheduler.java
@@ -26,7 +26,7 @@ public class NotificationScheduler {
     final private String BASE_MESSAGE = "일정있정에서 오늘 일정 안내드립니다.\n";
     final private String SCHEDULE_GUIDE = "[%s]\n%s - %s\n%s\n";
 
-    @Scheduled(cron=" 0 0 20 ? * * ", zone = "Asia/Seoul")
+    @Scheduled(cron=" 0 0 10 ? * * ", zone = "Asia/Seoul")
     private void searchTodaySchedules() {
         Date startToday = setTodayTime(0, 0, 0);
         Date endToday = setTodayTime(23, 59, 59);
